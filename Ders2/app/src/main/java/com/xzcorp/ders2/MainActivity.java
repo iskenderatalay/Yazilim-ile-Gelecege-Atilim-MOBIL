1d5b1f4058d83590b6bdf3990821e535adb65fab
iskender atalay <70414653+iskenderatalay@users.noreply.github.com>
2024-02-16 22:43:06 +0300

    Ders2 Eklendi
    Ders2 Eklendi

---
+++ Ders2/app/src/main/java/com/xzcorp/ders2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.xzcorp.ders2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mesajDegisken;
+    Button buttonDegisken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonDegisken = (Button)findViewById(R.id.kaydet);
+        mesajDegisken = (TextView)findViewById(R.id.mesaj);
+
+        buttonDegisken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mesajDegisken.setText("Text Değişti");
+            }
+        });
+
+        buttonDegisken.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this,"Uzun Bastın",Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        buttonDegisken.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                mesajDegisken.setText("Hover");
+            }
+        });
+    }
+}
